1750301eea033840e6872f8f75b9e6b248ead657
Kuba Orlik <groovy354@gmail.com>
2015-08-13 12:55:47 +0200

    bugfixes to pass sealious compatibility test

--- datastore.file.js
+++ datastore.file.js
@@ -1,4 +1,5 @@
 var fs = require("fs");
+var assert = require("assert");
 var path = require("path");
 var dot = require('dot-object'); //for transforming queries for easier parsing
 var merge = require("merge");
@@ -18,10 +19,10 @@ function collection_to_filename(collection_name){
 	return path.resolve(storage_dir_path + "/" + collection_name + ".json");
 }
 
-function document_matches_query(document, unprocessed_query){
+function document_matches_query(document, query){
 	//curently ignoring mongodb syntax's extras, like $gt etc. - we're just directly comparing two values
-	if(typeof unprocessed_query == "object" && typeof document=="object"){
-		var query = dot.object(unprocessed_query);
+	if(typeof query == "object" && typeof document=="object"){
+		dot.object(query);
 		for(var attribute_name in query){
 			var match = document_matches_query(document[attribute_name], query[attribute_name])
 			if(!match){
@@ -30,7 +31,7 @@ function document_matches_query(document, unprocessed_query){
 		}
 		return true;
 	}else{
-		return document==unprocessed_query;
+		return document==query;
 	}
 }
 
@@ -38,7 +39,8 @@ function read_from_file(filename){
 	if(!fs.existsSync(filename)){
 		fs.writeFileSync(filename, "[]");
 	}
-	return require(filename);
+	var ret = JSON.parse(fs.readFileSync(filename));
+	return ret;
 }
 
 function generate_sorting_function(sort_description){
@@ -65,11 +67,16 @@ function get_all_documents(collection_name){
 	return read_from_file(filename);
 }
 
-function apply_changes_to_document(document, unprocessed_changes){
+function apply_changes_to_document(document, changes){
 	//`changes` is an object describing what changes to apply to the document, using [MongoDB's update syntax](http://docs.mongodb.org/manual/reference/method/db.collection.update/#example-update-specific-fields)
 	//currently we only support the "$set" operator
-	var changes = dot.object(unprocessed_changes["$set"]);
-	return merge(document, changes);
+	dot.object(changes);
+	if(changes["$set"]){
+		//very temporary workaround until a library for parsing mongodb queries comes
+		changes = changes["$set"];
+	}
+	var ret = merge.recursive(document, changes);
+	return ret;
 
 }
 
@@ -109,22 +116,45 @@ FileDatastore.insert = function(collection_name, to_insert, options){
 
 FileDatastore.update = function(collection_name, query, new_value){
 	var all_entries = get_all_documents(collection_name);
-	all_entries.forEach(function(document, index){
+	all_entries.map(function(document, index){
 		if(document_matches_query(document, query)){
-			all_entries[index] = apply_changes_to_document(document, new_value);
+			var updated_document =  apply_changes_to_document(document, new_value);
+			return updated_document;
+		}else{
+			return document;
 		}
 	});
-	return Promise.resolve();
-};
-
-FileDatastore.remove = function(collection_name, query, just_one){
-	var all_entries = get_all_documents(collection_name);
-	var filtered_entries = all_entries.filter(function(document){
-		return document_matches_query(document, query);
-	})
 	var filename = collection_to_filename(collection_name);
 	write_to_file(filename, all_entries);
 	return Promise.resolve();
 };
 
-console.log(collection_to_filename("col"));
+FileDatastore.remove = function(collection_name, query, just_one){
+	var all_entries = get_all_documents(collection_name);
+	var filtered_entries = [];
+	if(just_one){
+		var deleted_amount = 0;
+		for(var i in all_entries){
+			var entry = all_entries[i];
+			if(!document_matches_query(entry, query) || deleted_amount>0){
+				filtered_entries.push(entry);
+			}else{
+				deleted_amount++;
+			}
+		}
+	}else{
+		filtered_entries = all_entries.filter(function(document){
+			return !document_matches_query(document, query);
+		})		
+	}
+	var filename = collection_to_filename(collection_name);
+	write_to_file(filename, filtered_entries);
+	return Promise.resolve();
+};
+
+FileDatastore.clear_collection = function(collection_name){
+	var filename = collection_to_filename(collection_name);
+	fs.writeFileSync(filename, "[]");
+}
+
+FileDatastore.test();
